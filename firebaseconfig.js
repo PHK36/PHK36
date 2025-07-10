4dd0db1a59837f5db55eed446848551c371726f1
PHK36 <veernewrank@gmail.com>
2025-07-10 11:22:15 +0530

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+  apiKey: "AIza....",
+  authDomain: "phk36.firebaseapp.com",
+  projectId: "phk36",
+  storageBucket: "phk36.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdefg12345"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
