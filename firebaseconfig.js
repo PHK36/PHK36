a78aae2e051d0eb6554f90c0cf571b2eb5f73b83
PHK36 <veernewrank@gmail.com>
2025-07-10 12:22:15 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -11,3 +11,18 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Y4Qem3HHW-MFOhV-ev1A3787-nRygpk",
+  authDomain: "phk36-6cbf9.firebaseapp.com",
+  projectId: "phk36-6cbf9",
+  storageBucket: "phk36-6cbf9.appspot.com",
+  messagingSenderId: "538514333070",
+  appId: "1:538514333070:web:ad59927d9a1db39abf98df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
